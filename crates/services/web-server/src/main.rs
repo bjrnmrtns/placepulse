b85d259c79a6618ee5544503458da5759ffac003
Bjorn Martens <bjorn@expeditious.nl>
2024-09-19 20:27:21 +0200

    add login handler check json for username pass value

--- crates/services/web-server/src/main.rs
+++ crates/services/web-server/src/main.rs
@@ -1,8 +1,40 @@
 use axum::extract::Path;
-use axum::response::{Html, IntoResponse};
-use axum::routing::get;
-use axum::Router;
+use axum::http::StatusCode;
+use axum::response::{Html, IntoResponse, Response};
+use axum::routing::{get, post};
+use axum::{Json, Router};
 use lib_utils::b64::b64u_encode;
+use serde::Deserialize;
+use serde::Serialize;
+use serde_json::{json, Value};
+
+pub const AUTH_TOKEN: &str = "auth-token";
+
+#[derive(Debug, Deserialize)]
+struct LoginPayload {
+    username: String,
+    pwd: String,
+}
+
+pub type Result<T> = core::result::Result<T, Error>;
+
+#[derive(Clone, Debug, Serialize, strum_macros::AsRefStr)]
+#[serde(tag = "type", content = "data")]
+pub enum Error {
+    LoginFail,
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> Response {
+        println!("->> {:<12} - {self:?}", "INTO_RES");
+
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        response.extensions_mut().insert(self);
+
+        response
+    }
+}
 
 async fn handler_hello(Path(name): Path<String>) -> impl IntoResponse {
     println!("->> {:<12} - handler_hello2 {name:?}", "HANDLER");
@@ -13,11 +45,34 @@ fn routes_hello() -> Router {
     Router::new().route("/hello/:name", get(handler_hello))
 }
 
+async fn api_login(payload: Json<LoginPayload>) -> Result<Json<Value>> {
+    println!("--> {:<12} - api_login", "HANDLER");
+
+    if payload.username != "demo1" || payload.pwd != "welcome" {
+        return Err(Error::LoginFail);
+    }
+
+    //TODO: Implement real auth-token generation/signature
+    //    cookies.add(Cookie::new(AUTH_TOKEN, "user-1.exp.sign"));
+
+    let body = Json(json!({
+        "result": {
+            "success": true
+        }
+    }));
+
+    Ok(body)
+}
+
+fn routes_login() -> Router {
+    Router::new().route("/api/login", post(api_login))
+}
+
 #[tokio::main]
 async fn main() {
     let encoded_data = b64u_encode([0, 1, 2, 3]);
     println!("{}", encoded_data);
-    let routes_all = Router::new().merge(routes_hello());
+    let routes_all = Router::new().merge(routes_hello()).merge(routes_login());
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, routes_all).await.unwrap();
 }
