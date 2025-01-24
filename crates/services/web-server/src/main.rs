35d694ffdc5c77729da66e09c24da07323a34f54
Bjorn Martens <borjn@proton.me>
2025-01-24 16:38:10 +0100

    simple askama / axum test

--- crates/services/web-server/src/main.rs
+++ crates/services/web-server/src/main.rs
@@ -1,37 +1,48 @@
-use axum::http::StatusCode;
-use axum::routing::{get, post};
-use axum::{Json, Router};
-use lib_utils::b64::b64u_encode;
-use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex};
+
+use askama::Template;
+use axum::{extract::State, response::{Html, IntoResponse}, routing::{get, post}, Router};
+
+#[derive(askama::Template)]
+#[template(path = "index.html")]
+struct RootTemplate {
+    counter: u32,
+}
+
+#[derive(askama::Template)]
+#[template(path = "counter.html")]
+struct CounterTemplate {
+    counter: u32,
+}
+
+#[derive(Clone)]
+struct AppState {
+    counter: u32,
+}
 
 #[tokio::main]
 async fn main() {
-    let encoded_data = b64u_encode([0, 1, 2, 3]);
-    println!("{}", encoded_data);
-    let app = Router::new().route("/", get(root)).route("/users", post(create_user));
+    let state = Arc::new(Mutex::new(AppState { counter: 0 }));
+    let app = Router::new().route("/", get(root)).route("/increment", post(increment)).with_state(state);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn root() -> &'static str {
-    "Hello World!"
-}
-
-async fn create_user(Json(payload): Json<CreateUser>) -> (StatusCode, Json<User>) {
-    let user = User {
-        id: 1337,
-        username: payload.username,
+async fn increment(State(state): State<Arc<Mutex<AppState>>>) -> impl IntoResponse {
+    let mut state = state.lock().unwrap();
+    state.counter += 1;
+    let template = CounterTemplate {
+        counter: state.counter,
     };
-    (StatusCode::CREATED, Json(user))
+    let html = template.render().unwrap();
+    Html(html).into_response()
 }
 
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
+async fn root(State(state): State<Arc<Mutex<AppState>>>) -> impl IntoResponse {
+    let template = RootTemplate {
+        counter: state.lock().unwrap().counter,
+    };
+    let html = template.render().unwrap();
+    Html(html).into_response()
 }
 
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
-}
